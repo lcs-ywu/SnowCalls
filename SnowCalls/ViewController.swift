475140c70f548e31623d4ee1d3b400f2f3fa2c3e
James Wu <ywu@lcs.on.ca>
2019-11-26 20:52:58 -0500

    Initial Commit

---
+++ SnowCalls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnowCalls
+//
+//  Created by James Wu on 2019-11-26.
+//  Copyright © 2019 James Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
